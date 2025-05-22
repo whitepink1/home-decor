00cc4aa2117d7fd7ea88fe19b783adf8edb6c205
whitepink1 <kiruhat9@gmail.com>
2025-05-22 10:10:49 +0300

    Updating readme  v1.1

--- app/_components/MainLink.js
+++ app/_components/MainLink.js
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MainLink({children, dest}) {
-    return(<Link className="text-basic-50 text-[16px] px-8 my-4" href={dest}>{children}</Link>)
+    const pathname = usePathname();
+    return(<Link className={`text-basic-50 text-[16px] px-8 py-2 my-2 ${pathname === dest ? 'bg-white/15' : ''}`} href={dest}>{children}</Link>)
 }
