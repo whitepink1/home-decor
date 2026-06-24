553f9e36e7341d6f7bf8463ca302e4f73ab40c63
kiruhat <kiruhat9@gmail.com>
2026-06-24 14:15:02 +0300

    fix: search params bug

--- app/shop/page.js
+++ app/shop/page.js
@@ -6,9 +6,9 @@ export const metadata = {
     title: "Shop",
   };
 
-  export default function Page({ searchParams }) {
-    const page = Number(searchParams.page) || 1;
-    const order = searchParams.order;
+  export default async function Page({ searchParams }) {
+    const page = await Number(searchParams.page) || 1;
+    const order = await searchParams.order;
     return (<main>
         <div className="flex justify-center h-[250px] items-center min-[1200px]:h-[420px] bg-[url('../public/main4.jpg')] bg-cover bg-center text-center align-middle">
             <h1 className="text-[50px] font-bold text-basic-50">Shop</h1>
